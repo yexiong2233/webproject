e95af0db438cc17f874cece3aaafe185163bb7d9
yexiong2233 <s19t15916@stu.edu.tw>
2021-06-04 16:57:42 +0800

    add

---
+++ helloMVC/public/index.php
@@ -0,0 +1,9 @@
+<?php
+// 設定應用程式目錄為當前目錄
+define('APP_PATH', __DIR__.'/');
+// 開啟除錯模式
+define('APP_DEBUG', true);
+// 網站根目錄
+define('APP_URL', 'http://hellomvc');
+// 載入框架
+require './kernal/kernal.php';
